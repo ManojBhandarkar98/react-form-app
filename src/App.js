3c5c14a615a049e26c28d1b1ce0c721b5b1593c0
ManojBhandarkar98 <manojbhandarkar98@gmail.com>
2025-01-11 16:19:39 +0530

    Epi - 07 - Handling MultiState using single usestate with Object instead of multiple useState hook using empty string

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  // const [firstName, setFirstName] = useState("");
+  // const [lastName, setLastName] = useState("");
+
+  // function firstNameHandler(event) {
+  //   setFirstName(event.target.value)
+  // }
+  // function lastNameHandler(event) {
+  //   setLastName(event.target.value)
+  // }
+
+  //Handling Multiple state
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email:""
+  });
+
+  function changeHandler(event) {
+    setFormData(prevFormData => {
+      return {
+        ...prevFormData,
+        [event.target.name] : event.target.value
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>First Name : </label>
+        <input type='text' placeholder='first name' onChange={changeHandler} name='firstName' /><br/>
+        <label>Last Name : </label>
+        <input type='text' placeholder='last name' onChange={changeHandler} name='lastName' /><br/>
+        <label>Email : </label>
+        <input type='email' placeholder='enter your email here' onChange={changeHandler} name='email' />
+      </form>
     </div>
   );
 }
